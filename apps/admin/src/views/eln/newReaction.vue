0a2c97de19b017859e11a3d81b2edaacfa824ef9
cx-luo <zibuyu886@sina.cn>
2025-06-27 14:58:28 +0800

    init: init ketcher.

---
+++ apps/admin/src/views/eln/newReaction.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {Page} from "@vben/common-ui";
+</script>
+
+<template>
+  <Page title="Create a reaction">
+    <ElCard>
+      <div id="marvin-js" style="margin-top: 10px">
+        <iframe
+          id="ketcher-js-editor"
+          src="/static/ketcher/index.html"
+          width="100%"
+          height="450px"
+        ></iframe>
+      </div>
+    </ElCard>
+  </Page>
+</template>
+
+<style scoped>
+
+</style>
